00c11d639bb8aed67520a9cb773570b3a6ae4aba
song.yue <yuesong@gmail.com>
2026-04-09 14:40:40 +0800

    fix: replace Carbon hotkeys with CGEvent tap for background app compatibility

--- HumanTypist/Core/HotkeyManager.swift
+++ HumanTypist/Core/HotkeyManager.swift
@@ -1,27 +1,18 @@
 import Foundation
-import Carbon
+import CoreGraphics
+import AppKit
 
 final class HotkeyManager {
 
     static let shared = HotkeyManager()
 
-    private var hotkeyRefStart: EventHotKeyRef?
-    private var hotkeyRefStop: EventHotKeyRef?
-    private var hotkeyRefReload: EventHotKeyRef?
-    private var eventHandler: EventHandlerRef?
+    private var eventTap: CFMachPort?
+    private var runLoopSource: CFRunLoopSource?
+    private var isRegistered = false
 
     var onStart: (() -> Void)?
     var onStop: (() -> Void)?
     var onReload: (() -> Void)?
-
-    private(set) var isRegistered = false
-
-    // Ctrl+Alt+P = P keycode 0x35 (35), modifiers control+option
-    // Ctrl+Alt+S = S keycode 0x01 (1), modifiers control+option
-    // Ctrl+Alt+R = R keycode 0x0F (15), modifiers control+option
-    private let startKeyID = EventHotKeyID(signature: OSType(0x48545043), id: 1) // "HTPC"
-    private let stopKeyID = EventHotKeyID(signature: OSType(0x48545043), id: 2)
-    private let reloadKeyID = EventHotKeyID(signature: OSType(0x48545043), id: 3)
 
     private init() {}
 
@@ -32,114 +23,81 @@ final class HotkeyManager {
         self.onStop = onStop
         self.onReload = onReload
 
-        var eventType = EventTypeSpec(eventClass: OSType(kEventClassKeyboard), eventKind: UInt32(kEventHotKeyPressed))
+        // Ctrl+Alt+P = keycode 0x35, modifiers: control + option
+        let startKey = KeyCombo(keyCode: 0x35, modifiers: 0x1100) // controlKey | optionKey
+        // Ctrl+Alt+S = keycode 0x01
+        let stopKey = KeyCombo(keyCode: 0x01, modifiers: 0x1100)
+        // Ctrl+Alt+R = keycode 0x0F
+        let reloadKey = KeyCombo(keyCode: 0x0F, modifiers: 0x1100)
 
-        let eventTarget = GetEventDispatcherTarget()
+        let eventMask = (1 << CGEventType.keyDown.rawValue)
 
-        let status = InstallEventHandler(
-            eventTarget,
-            { (_, event, _) -> OSStatus in
-                HotkeyManager.shared.handleHotkey(event)
-                return noErr
+        let refcon = UnsafeMutableRawPointer(Unmanaged.passUnretained(self).toOpaque())
+
+        guard let tap = CGEvent.tapCreate(
+            tap: .cgSessionEventTap,
+            place: .headInsertEventTap,
+            options: .defaultTap,
+            eventsOfInterest: CGEventMask(eventMask),
+            callback: { (proxy, type, event, refcon) -> Unmanaged<CGEvent>? in
+                guard let refcon = refcon else { return Unmanaged.passRetained(event) }
+                let manager = Unmanaged<HotkeyManager>.fromOpaque(refcon).takeUnretainedValue()
+                manager.handleEvent(proxy: proxy, type: type, event: event)
+                return Unmanaged.passRetained(event)
             },
-            1,
-            &eventType,
-            nil,
-            &eventHandler
-        )
-
-        guard status == noErr else {
-            print("[HotkeyManager] InstallEventHandler failed: \(status)")
+            userInfo: refcon
+        ) else {
+            print("[HotkeyManager] CGEvent.tapCreate FAILED - need Accessibility permission")
             return
         }
-        print("[HotkeyManager] EventHandler installed OK")
 
-        // Ctrl+Alt+P = P keycode 0x35 (35), modifiers control+option
-        var startHotkeyID = startKeyID
-        let startStatus = RegisterEventHotKey(
-            0x35, // kVK_ANSI_P
-            UInt32(controlKey | optionKey),
-            startHotkeyID,
-            eventTarget,
-            0,
-            &hotkeyRefStart
-        )
-        print("[HotkeyManager] RegisterEventHotKey P: \(startStatus)")
+        eventTap = tap
+        runLoopSource = CFMachPortCreateRunLoopSource(kCFAllocatorDefault, tap, 0)
+        CFRunLoopAddSource(CFRunLoopGetMain(), runLoopSource, .commonModes)
+        CGEvent.tapEnable(tap: tap, enable: true)
 
-        // Ctrl+Alt+S = S keycode 0x01 (1), modifiers control+option
-        var stopHotkeyID = stopKeyID
-        let stopStatus = RegisterEventHotKey(
-            0x01, // kVK_ANSI_S
-            UInt32(controlKey | optionKey),
-            stopHotkeyID,
-            eventTarget,
-            0,
-            &hotkeyRefStop
-        )
-        print("[HotkeyManager] RegisterEventHotKey S: \(stopStatus)")
-
-        // Ctrl+Alt+R = R keycode 0x0F (15), modifiers control+option
-        var reloadHotkeyID = reloadKeyID
-        let reloadStatus = RegisterEventHotKey(
-            0x0F, // kVK_ANSI_R
-            UInt32(controlKey | optionKey),
-            reloadHotkeyID,
-            eventTarget,
-            0,
-            &hotkeyRefReload
-        )
-        print("[HotkeyManager] RegisterEventHotKey R: \(reloadStatus)")
-
+        print("[HotkeyManager] Event tap enabled successfully")
         isRegistered = true
     }
 
+    private func handleEvent(proxy: CGEventTapProxy, type: CGEventType, event: CGEvent) {
+        guard type == .keyDown else { return }
+
+        let keyCode = event.getIntegerValueField(.keyboardEventKeycode)
+        let flags = event.flags
+
+        // Check for Ctrl+Option modifiers
+        let hasControl = flags.contains(.maskControl)
+        let hasOption = flags.contains(.maskAlternate)
+
+        guard hasControl && hasOption else { return }
+
+        print("[HotkeyManager] Key pressed: keycode=\(keyCode) ctrl=\(hasControl) opt=\(hasOption)")
+
+        switch keyCode {
+        case 0x35: // P
+            DispatchQueue.main.async { self.onStart?() }
+        case 0x01: // S
+            DispatchQueue.main.async { self.onStop?() }
+        case 0x0F: // R
+            DispatchQueue.main.async { self.onReload?() }
+        default:
+            break
+        }
+    }
+
     func unregister() {
-        if let ref = hotkeyRefStart {
-            UnregisterEventHotKey(ref)
-            hotkeyRefStart = nil
-        }
-        if let ref = hotkeyRefStop {
-            UnregisterEventHotKey(ref)
-            hotkeyRefStop = nil
-        }
-        if let ref = hotkeyRefReload {
-            UnregisterEventHotKey(ref)
-            hotkeyRefReload = nil
-        }
-        if let handler = eventHandler {
-            RemoveEventHandler(handler)
-            eventHandler = nil
+        if let tap = eventTap {
+            CGEvent.tapEnable(tap: tap, enable: false)
+            CFRunLoopRemoveSource(CFRunLoopGetMain(), runLoopSource, .commonModes)
+            eventTap = nil
+            runLoopSource = nil
         }
         isRegistered = false
     }
+}
 
-    private func handleHotkey(_ event: EventRef?) -> OSStatus {
-        guard let event = event else { return OSStatus(eventNotHandledErr) }
-        var hotkeyID = EventHotKeyID()
-        let status = GetEventParameter(
-            event,
-            UInt32(kEventParamDirectObject),
-            UInt32(typeEventHotKeyID),
-            nil,
-            MemoryLayout<EventHotKeyID>.size,
-            nil,
-            &hotkeyID
-        )
-        guard status == noErr else {
-            print("[HotkeyManager] GetEventParameter failed: \(status)")
-            return status
-        }
-
-        print("[HotkeyManager] Hotkey fired, id: \(hotkeyID.id)")
-
-        DispatchQueue.main.async {
-            switch hotkeyID.id {
-            case 1: self.onStart?()
-            case 2: self.onStop?()
-            case 3: self.onReload?()
-            default: break
-            }
-        }
-        return noErr
-    }
+struct KeyCombo {
+    let keyCode: Int64
+    let modifiers: Int64
 }
